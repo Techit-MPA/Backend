9fb7b46bea5c897f7e4e8f9c22c30ea3ab30465d
ranunclulus <morion002@gmail.com>
2023-08-07 17:16:04 +0900

    :sparkles: feat: <프로젝트 생성>

---
+++ src/test/java/com/example/chatfia_be/ChatfiaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatfia_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatfiaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
